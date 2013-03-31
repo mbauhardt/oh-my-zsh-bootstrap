3b4b3189623c640f519d844d86c0a86adc12345a
Marko Bauhardt <marko.bauhardt@googlemail.com>
2013-04-01 00:13:54 +0200

    implement enable/download theme

--- lib/download.zsh
+++ lib/download.zsh
@@ -12,3 +12,10 @@ download_plugin() {
   _clone_git $url $ZSH_CUSTOM/plugins/$name
   _map_put plugins $name enabled
 }
+
+download_theme() {
+  [[ "$#" != 2 ]] && return 1
+  local url=$1; local theme=$2;
+  _clone_git $url $ZSH_CUSTOM/themes/$theme
+  _map_put themes theme $theme
+}
--- lib/repository.zsh
+++ lib/repository.zsh
@@ -7,6 +7,11 @@ _init_plugins() {
   done
 }
 
+_init_theme() {
+  _map_exists themes theme
+  [[ $? -ne 0 ]] && _map_put themes theme robbyrussell
+}
+
 _pre_enable_plugins() {
   for plugin ($plugins); do
     _map_put plugins $plugin pre_enabled
@@ -24,6 +29,18 @@ list_plugins() {
   done
 }
 
+list_themes() {
+  for theme ($ZSH/themes/*); do
+    local theme_name=$(basename $theme | awk -F '.' '{print $1}')
+    local selected_theme=$(_map_get themes theme)
+    if [[ $selected_theme = $theme_name ]]; then
+      printf "%-30s \033[0;32m%-10s\033[0m\n" $theme_name enabled
+    else
+      printf "%-30s \033[0;30m%-10s\033[0m\n" $theme_name disabled
+    fi
+  done
+}
+
 list_enabled_plugins() {
   for plugin ($(_map_keys plugins)); do
     local enabled=$(_map_get plugins $plugin)
@@ -36,6 +53,12 @@ enable_plugin() {
   local plugin=$1
   local enabled=$(_map_get plugins $plugin)
   [[ $enabled = "disabled" ]] && _map_put plugins $plugin enabled
+}
+
+enable_theme() {
+  [[ "$#" != 1 ]] && return 1
+  local theme=$1
+  _map_put themes theme $theme
 }
 
 disable_plugin() {
@@ -57,7 +80,13 @@ _populate_enabled_plugins() {
   done
 }
 
+_populate_enabled_theme() {
+  ZSH_THEME=$(_map_get themes theme)
+}
+
 _init_plugins $ZSH/plugins
 _init_plugins $ZSH_CUSTOM/plugins
 _pre_enable_plugins
 _populate_enabled_plugins
+_init_theme
+_populate_enabled_theme
